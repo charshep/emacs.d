c4db09748d39a09abecacd61705b1a985787540d
Charlie Sheppard <charlie.sheppard@veeva.com>
2018-05-16 10:55:25 -0700

    Setting auto-revert-check-vc-info so that modeline gets updated on branch checkout. May be too CPU intensive so may revert in future.

--- init.el
+++ init.el
@@ -266,6 +266,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-check-vc-info t)
  '(blink-cursor-mode nil)
  '(c-default-style
    (quote
